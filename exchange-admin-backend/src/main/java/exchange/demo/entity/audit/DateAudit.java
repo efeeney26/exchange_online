2466bbd84c04a83cfc980f7d3c30e522b340b193
Danil <danilkochkin22@gmail.com>
2019-07-09 01:44:08 +0300

    add(app config) :: entity, repository

---
+++ exchange-admin-backend/src/main/java/exchange/demo/entity/audit/DateAudit.java
@@ -0,0 +1,28 @@
+package exchange.demo.entity.audit;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.*;
+import org.springframework.data.annotation.*;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.time.Instant;
+
+@MappedSuperclass
+@Getter
+@Setter
+@EntityListeners(AuditingEntityListener.class)
+@JsonIgnoreProperties(
+		value = {"createdAt", "updatedAt"},
+		allowGetters = true
+)
+public abstract class DateAudit implements Serializable {
+	@CreatedDate
+	@Column(nullable = false, updatable = false)
+	private Instant createdAt;
+
+	@LastModifiedDate
+	@Column(nullable = false)
+	private Instant updatedAt;
+}
---
+++ exchange-admin-backend/src/main/java/exchange/demo/repository/RoleRepository.java
@@ -0,0 +1,13 @@
+package exchange.demo.repository;
+
+import exchange.demo.entity.Role;
+import exchange.demo.entity.RoleName;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+	Optional<Role> findByName(RoleName roleName);
+}
